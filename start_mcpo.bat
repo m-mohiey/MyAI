3d1b1cc4c89a97b2aaf64429f9583a803708387e
m-mohiey <m.mohiey@gmail.com>
2025-05-04 14:44:52 +0300

    script to backup Claude desktop config (MCP) and start mcpo proxy on host

---
+++ start_mcpo.bat
@@ -0,0 +1,3 @@
+copy %APPDATA%\Claude\claude_desktop_config.json .
+@REM uvx mcpo --port 1000 --config claude_desktop_config.json
+@REM uvx mcpo --port 1000 --config %APPDATA%\Claude\claude_desktop_config.json
